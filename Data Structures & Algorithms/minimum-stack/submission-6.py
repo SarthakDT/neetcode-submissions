544cdd74d599d7c2875af322a2ea7e01ec3ea55e
Sarthak Srivastava <154724152+SarthakDT@users.noreply.github.com>
2026-08-16 16:06:01 +0530

    Add: minimum-stack - submission-6

---
+++ Data Structures & Algorithms/minimum-stack/submission-6.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack:
+            self.minStack.append(val)
+        
+        elif val <= self.minStack[-1]:
+            self.minStack.append(val)
+        
+    def pop(self) -> None:
+        y = self.stack.pop()
+        if y == self.minStack[-1]:
+            self.minStack.pop()
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.minStack:
+            return self.minStack[-1]
+        
